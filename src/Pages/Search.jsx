8ce61e708514048e52c37fa30fdb7e626a5c7f95
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-11-12 15:23:26 -0300

    Corrige: bugs de renderização

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,45 +1,52 @@
-import React, { useContext, useEffect, useState } from 'react'
-import context from '../Services/Context'
+import React, { useContext, useEffect, useState } from "react";
+import context from "../Services/Context";
 
 export default function Search() {
-  const { location, setLocation} = useContext(context)
-    const [valueInput, setValueInput] = useState('')
-    const [ disable, setDisable ] = useState([])
-       
+  const { location, setLocation } = useContext(context);
+  const [valueInput, setValueInput] = useState("");
+  const [disable, setDisable] = useState(false);
 
-    const handleClick = async () => {
-        const urlWeather = `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&lang=pt_br,uk&APPID=6c65d4b916ef01e6bd8bec09df51d9b1`
-        const response = await fetch(urlWeather)
-        const data = await response.json()
-        setLocation(data)
-        setDisable(true)
-    }
-        
+  const handleClick = async () => {
+    const urlWeather = `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&lang=pt_br,uk&APPID=6c65d4b916ef01e6bd8bec09df51d9b1`;
+    const response = await fetch(urlWeather);
+    const data = await response.json();
+    setLocation(data);
+    setDisable(true);
+  };
 
+  useEffect(() => {
+    setDisable(false);
+  }, []);
 
-
-const handleChange = ({target}) => {
-    setValueInput(target.value)
-}
-console.log(location)
-const urlCountry = `https://countryflagsapi.com/png/${location.sys.country}`
+  const handleChange = ({ target }) => {
+    setValueInput(target.value);
+  };
   return (
     <div>
-        <input type="text" placeholder='Digite a cidade' onChange={handleChange} />
-        <button type="button" onClick={handleClick}>Pesquisar</button>
-        {disable &&
-      <section>
-        <p>{location.name}</p>
-        <img alt={urlCountry} src={urlCountry}></img>
+      <input
+        type="text"
+        placeholder="Digite a cidade"
+        onChange={handleChange}
+      />
+      <button type="button" onClick={handleClick}>
+        Pesquisar
+      </button>
+      {disable && (
+        
+        <section>
+          <p>{location.name}</p>
+          <img className="country" alt={location.sys.country} src={`https://countryflagsapi.com/png/${location.sys.country}`}></img>
 
-        <ul>
-          Latitude
-          <li>{location.coord.lat}</li>
-          Longitude
-          <li>{location.coord.lon}</li>
-        </ul>
+          <ul>
+          Temperatura ºC
+            <p>{(location.main.temp)}</p>
+            Latitude
+            <li>{location.coord.lat}</li>
+            Longitude
+            <li>{location.coord.lon}</li>
+          </ul>
         </section>
-}
+      )}
     </div>
-  )
+  );
 }
